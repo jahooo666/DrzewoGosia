e8d48620ea65fd35ed655382fd565d5aed01e5b1
jahooo666 <okraskaj@ee.pw.edu.pl>
2015-11-29 02:25:14 +0100

    >zmodyfikowana klasa node- dodawanie za pomoca łańcucha znaków

--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,91 @@
 public class Node {
+    private char label;
+    private Node left, right;
 
+    public Node(char label) {
+        this.label = label;
+        this.left = null;
+        this.right = null;
+    }
+
+    public char getLabel() {
+        return label;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+    public void print() {
+        if (this.right != null)
+            this.right.print();
+        System.out.println(label);
+        if (this.left != null)
+            this.left.print();
+    }
+
+    public void printFormated(int level) {
+        if (this.right != null)
+            this.right.printFormated(level + 1);
+
+        String spaces = "";
+        for (int i = 0; i < level; i++)
+            spaces = spaces + "  ";
+
+        System.out.println(spaces + label);
+
+        if (this.left != null)
+            this.left.printFormated(level + 1);
+    }
+
+    public void insert(char label, String position) throws Exception {
+        if (position.length() > 0) {
+            if (position.charAt(0) == 'L') {
+                if (this.left == null) {
+                    this.left = new Node(label);
+                } else
+                    this.left.insert(label, position.substring(1));
+            } else if (position.charAt(0) == 'R') {
+                if (this.right == null) {
+                    this.right = new Node(label);
+                } else
+                    this.right.insert(label, position.substring(1));
+            } else
+                throw new Exception("Nieprawidłowa składnia: Pozycja powinna składać się wyłączeni ze znaków P oraz L");
+        } else {
+            //dodajemy w tym miejscu element
+            System.out.println("Coś poszło nie tak, ale nie do końca wiemy co");
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+   /* public double get(int index) {
+        if (index == this.index) {
+            return this.value;
+        } else if (index < this.index) {
+            if (this.left == null)
+                return 0.0;
+            else
+                return this.left.get(index);
+        } else if (index > this.index) {
+            if (this.right == null)
+                return 0.0;
+            else
+                return this.right.get(index);
+        }
+        return 0;
+    }*/
 }
